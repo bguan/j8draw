a3814729f29b8585190746a41affe1b49eb2cb08
Brian Guan <brian@guan.us>
2021-03-21 20:58:26 -0700

    Finetuning tree

--- src/jdraw/Tree2DFX.java
+++ src/jdraw/Tree2DFX.java
@@ -46,14 +46,14 @@ public class Tree2DFX extends Application {
 	// constants useful for logic
 	final Point2D ORIGIN = new Point2D(MAX_WIDTH / 2, .9 * MAX_HEIGHT);
 	final double ANGLE_SCALE = 1;
-	final double LENGTH_SCALE = 0.85;
+	final double LENGTH_SCALE = 0.88;
 
 	// draw a Tree by making initial call to drawBranch to start recursion chain
 	void drawTree() {
-		double initAngL = 45 * Math.PI / 180;
-		double initAngR = 15 * Math.PI / 180;
+		double initAngL = 30 * Math.PI / 180;
+		double initAngR = 24 * Math.PI / 180;
 		double initLen = -100;
-		int maxBranchDepth = 13;
+		int maxBranchDepth = 14;
 		drawBranch(ORIGIN, 0, initAngL, initAngR, initLen, maxBranchDepth);
 	}
 
@@ -74,10 +74,11 @@ public class Tree2DFX extends Application {
 		System.out.println(dep + ": draw from " + pos + " to " + nextPos);
 
 		gc.setLineWidth(dep);
-		gc.setStroke(Color.rgb(0, 255/dep, 0));
+		gc.setStroke(Color.rgb(0, Math.max(10, 255 - 20*dep), 0));
 		gc.strokeLine(pos.getX(), pos.getY(), nextPos.getX(), nextPos.getY());
 
-		if (dep%3==1 && Math.random() > .95) drawApple(nextPos, 2*dep+1, Color.RED);
+		if (dep%3==1 && Math.random() > .99) 
+			drawApple(nextPos, dep+2);
 
 		double nextDirLeft = dir - turnL;
 		double nextDirRight = dir + turnR;
@@ -90,9 +91,12 @@ public class Tree2DFX extends Application {
 	}
 
 	// draw an apple at the position of radius
-	void drawApple(Point2D pos, double radius, Color c) {
-		gc.setFill(c);
-		gc.fillOval(pos.getX() - radius, pos.getY() - radius, 2 * radius, 2 * radius);
+	void drawApple(Point2D pos, double radius) {
+		gc.setLineWidth(1);
+		gc.setStroke(Color.BROWN);
+		gc.arcTo(pos.getX(), pos.getY(), pos.getX()+radius, pos.getY()+radius, 4*radius);
+		gc.setFill(Color.rgb((255 - 3*(int)radius),0,0));
+		gc.fillOval(pos.getX(), pos.getY(), 2 * radius, 2 * radius);
 	}
 
 }
