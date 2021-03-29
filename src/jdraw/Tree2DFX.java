f8dc9249ce6b4ddd771c80ce8ce13b68679af173
bguan <brian.guan@gmail.com>
2021-03-29 13:11:15 -0700

    Added middle branch and more random noise to mimic fennel.

--- src/jdraw/Tree2DFX.java
+++ src/jdraw/Tree2DFX.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.StrokeLineCap;
 import javafx.stage.Stage;
 
 public class Tree2DFX extends Application {
@@ -45,22 +46,27 @@ public class Tree2DFX extends Application {
 
 	// constants useful for logic
 	final Point2D ORIGIN = new Point2D(MAX_WIDTH / 2, .9 * MAX_HEIGHT);
-	final double ANGLE_SCALE = 1;
-	final double LENGTH_SCALE = 0.88;
+	final double ANGLE_SCALE = 0.8;
+	final double LENGTH_SCALE = 0.8;
 
 	// draw a Tree by making initial call to drawBranch to start recursion chain
 	void drawTree() {
-		double initAngL = 30 * Math.PI / 180;
+		double initAngL = 36 * Math.PI / 180;
 		double initAngR = 24 * Math.PI / 180;
 		double initLen = -100;
-		int maxBranchDepth = 14;
+		int maxBranchDepth = 6;
 		drawBranch(ORIGIN, 0, initAngL, initAngR, initLen, maxBranchDepth);
 	}
 
+	double weightedRandom(double weight) {
+		return (1.0 - weight/2) + Math.random()*weight;
+	}
+	
 	// draw a branch starting at pos, pointing at direction dir, length of len,
-	// then draw 2 branches left and right from there, by +/- turn from dir,
-	// branching recursively up to depth level
-	void drawBranch(Point2D pos, double dir, double turnL, double turnR, double len, int dep) {
+	// 2 branches from there by turning left and right by resp. angles turnL & turnR,
+	// each time adjusting branch length and angles by a scaling factor incl. some random 
+	// noise, branching recursively up to dep level
+	void drawBranch(Point2D pos, double dirM, double dirL, double dirR, double len, double dep) {
 
 		// if we've descended down to depth of 0 (or less) terminate recursion
 		if (dep <= 0)
@@ -69,25 +75,30 @@ public class Tree2DFX extends Application {
 		// draw a line of length from pos at angle (0' is Up, +ve is right)
 		// Use trigonometry to find next X, next Y
 
-		Point2D nextPos = new Point2D(pos.getX() + len * Math.sin(dir), pos.getY() + len * Math.cos(dir));
+		Point2D nextPos = new Point2D(pos.getX() + len * Math.sin(dirM), pos.getY() + len * Math.cos(dirM));
 
 		System.out.println(dep + ": draw from " + pos + " to " + nextPos);
 
 		gc.setLineWidth(dep);
-		gc.setStroke(Color.rgb(0, Math.max(10, 255 - 20*dep), 0));
+		gc.setStroke(Color.rgb(0, Math.max(10, 255 - 20*(int)dep), 0));
+		gc.setLineCap(StrokeLineCap.ROUND);
 		gc.strokeLine(pos.getX(), pos.getY(), nextPos.getX(), nextPos.getY());
 
-		if (dep%3==1 && Math.random() > .99) 
+		if (Math.random() > .99) 
 			drawApple(nextPos, dep+2);
 
-		double nextDirLeft = dir - turnL;
-		double nextDirRight = dir + turnR;
-		double nextLength = len * LENGTH_SCALE * (.75 + Math.random()/4);
-		double nextTurnL = turnL * ANGLE_SCALE * (.75 + Math.random()/4);
-		double nextTurnR = turnR * ANGLE_SCALE * (.75 + Math.random()/4);
+		double nextDirL = dirM - dirL;
+		double nextDirR = dirM + dirR;
+		double nextDirM = (nextDirL+nextDirR)* weightedRandom(.2)/2;
+		double nextLenL = len * LENGTH_SCALE * weightedRandom(.2);
+		double nextLenR = len * LENGTH_SCALE * weightedRandom(.2);
+		double nextLenM = len * Math.sqrt(LENGTH_SCALE)* weightedRandom(.1);
+		double nextTurnL = dirL * ANGLE_SCALE * weightedRandom(.2);
+		double nextTurnR = dirR * ANGLE_SCALE * weightedRandom(.2);
 
-		drawBranch(nextPos, nextDirLeft, nextTurnL, nextTurnR, nextLength, dep - 1);
-		drawBranch(nextPos, nextDirRight, nextTurnL, nextTurnR, nextLength, dep - 1);
+		drawBranch(nextPos, nextDirL, nextTurnL, nextTurnR, nextLenL, dep - weightedRandom(.5));
+		drawBranch(nextPos, nextDirR, nextTurnL, nextTurnR, nextLenR, dep - weightedRandom(.5));
+		drawBranch(nextPos, nextDirM, nextTurnL, nextTurnR, nextLenM, dep - weightedRandom(.5)/2);
 	}
 
 	// draw an apple at the position of radius
